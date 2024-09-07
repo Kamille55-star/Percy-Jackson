8fb3f928277d91246c12609081738fa8df25825a
Kamille55-star <kamillerodriguesdeoliveira@gmail.com>
2024-09-07 15:52:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function pesquisar () {
+    
+  let section = document.getElementById("resultados-pesquisa");
+  
+  let campoPesquisa = document.getElementById ("campo-pesquisa").value
+  
+  
+  if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nenhum mito encontrado</p>"
+    return
+  }
+  
+
+campoPesquisa = campoPesquisa.toLowerCase()
+  
+  
+  let resultados = "";
+  let titulo = ""
+  
+  for (let dado of dados) { 
+    titulo= dado.titulo.toLowerCase()
+    if (titulo.includes(campoPesquisa)|| dado.descricao.includes(campoPesquisa)){
+      
+      resultados += `
+    <div class="item-resultado">
+    <img src=${dado.imagem} alt="">
+              <h2> 
+                <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+          <p class = "descricao">${dado.descricao}</p>
+              <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+    ` 
+  }
+    
+    }
+
+    if (!resultados) {
+      resultados = "<p> O personagem escolhido ainda não foi adicionado.</p>"
+  
+    }
+  
+    
+    
+    section.innerHTML = resultados
+  }
+  
+  
---
+++ dados.js
@@ -0,0 +1,132 @@
+let dados = [
+    {
+      titulo: "Acampamento meio-sangue",
+      descricao: "Acampamento grego para semideuses, um dos poucos locais seguros para os semideuses. É um local de treinamento para os semideuses e não possui um sistema de organização de lider, o cargo mais alto que há é o líder da cabana, diferentemente do acampamento romano.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Acampamento_Meio-Sangue"
+    },
+    {
+      titulo: "Acampamento Júpiter",
+      descricao: "Acampamento romano para semideuses, um dos poucos locais seguros para os semideuses. É um local de treinamento para os semideuses e possui um sistema bem organizado de liderança, sendo composto por: Probatio, Legionário, Centurião, Senador, Pretor e, Cônsul.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Acampamento_J%C3%BApiter"
+    },
+   {
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQikw0ZOH90jYYXdkTqQ-q_Sl2zDaKWZpAIE-3JUdXwpXIR5C0CybT_WK3pacjE6aeI954&usqp=CAU",
+      titulo: "Percy Jackson",
+      descricao: "Percy Jackson é filho da mortal Sally Jackson e do deus Poseidon. É um semideus com poderes envolvendo a água, cavalos e terremotos, sendo capaz de causar grandes terremotos, controlar a água e se manter seco ou até mesmo de respirar na água e de falar com cavalos ou animais próximos a cavalos, como zebras e pégasos. Seu defeito fatal é a lealdade.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Percy_Jackson"
+    },
+    {
+      imagem: "https://i.pinimg.com/736x/05/0b/04/050b046c58b99a7fe69e2481c4a94675.jpg",
+      titulo: "Annabeth Chase",
+      descricao: "Annabeth Chase é filha da deusa Atena e do mortal Frederick Chase. Possui grande inteligência e estratégia em batalha, e no começo da saga não se dá bem com o pai. Seu defeito fatal é o orgulho.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Annabeth_Chase"
+    },
+    {
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTup87UwlCcBfAMRLkH9Bggv3FExvCjAG_VFw&s",
+      titulo: "Thalia Grace",
+      descricao: "Thalia Grace é filha da mortal Beryl Grace e do deus Zeus. Aos 12 anos foi transformada em um pinheiro quando ficou para trás para salvar Annabeth e Luke. Possui medo de altura e atualmente é uma caçadora de Ártemis. Possui a habilidade de controlar a névoa e a eletricidade. Seu defeito fatal é a ambição.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Thalia_Grace"
+    },
+    {
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT02DYv88xf3pVlQ4XDxSfP46pDa8K0GvHMWQ&s",
+      titulo: "Clarisse La Rue",
+      descricao: "Filha do deus da guerra Ares com uma mortal desconhecida. É líder do chalé de Ares e gosta bastante de brigar. Demorou bastante para virar amiga do Percy. Seu defeito fatal provavelmente é o orgulho. Interesse amoroso: Chris Rodríguez.",
+      link: "https://aminoapps.com/c/mitologicpt/page/item/clarisse-la-rue/mq8j_8JF0Ie0d7bKQMzPpzrga144DraeQ1"
+    },
+    {
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQbocEOIRUb068YL1radhLjNQUAkW3kSuH6Q&s",
+      titulo: "Poseidon",
+      descricao: "Deus dos mares, terremotos, cavalos, lagos, rios, tempestades, secas, inundações. É pai de Percy Jackson e rival de Athena. Contraparte romano: Netuno.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Poseidon"
+    },
+    {
+      imagem: "https://cdn.rickriordan.com/wp-content/uploads/2017/04/11230243/athenalongerguard-299x416.jpg",
+      titulo: "Athena/Atena",
+      descricao: "Deusa da sabedoria e da estratégia em guerras, da civilização, matemática, estratégia, guerra defensiva, do artesanato, das artes, habilidades, da inteligência e do brilhantismo, seu símbolo é uma coruja. Teve uma desavença com Aracne e depois disso todos os seus filhos adquiriram medo de aranhas. Olhos cinzentos e calculistas. Não gosta do Percy. Contraparte romano: Minerva.",
+      link: "https://riordan.fandom.com/wiki/Athena",
+    },
+    {
+      imagem: "https://i.pinimg.com/564x/d7/ae/72/d7ae724aeb0af8b702e4d75008539d84.jpg",
+      titulo: "Dionísio",
+      descricao: "Deus do vinho, do teatro, da loucura, das festas e da alegria. Foi condenado a ficar no acampamento meio-sangue e foi proibido de beber vinho após perseguir uma ninfa duas vezes. Contraparte romano: Baco.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Dion%C3%ADsio"
+    },
+    {
+      imagem: "https://static.wikia.nocookie.net/8f4d3252-c964-4d1b-90b7-a880c744b6ef/scale-to-width/755",
+      titulo: "Héstia",
+      descricao: "Deusa da lareira, do fogo, do calor, do lar, da família, da vida doméstica, das oferendas, do estado e da crença. A mais velha dos 12 Olimpianos, ela desistiu de seu lugar no Olimpo. A atual guardiã de Elpis, o espírito da esperança. Contraparte romana: Vesta.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/H%C3%A9stia"
+    },
+    {
+      imagem: "https://i.pinimg.com/originals/72/84/3d/72843d89d4b180b5de2016af10d75001.jpg",
+      titulo: "Grover",
+      descricao: "Grover foi o sátiro designado para cuidar de Thalia Grace e de Percy Jackson, se tornando um grande amigo do filho de Poseidon. Foi o primeiro e único buscador a encontrar Pã e se tornou membro do Conselho dos Anciãos de Casco Fendido. Pode dar bençãos aos animais, ler emoções, sentir cheiros de mosntros, deuses, semideuses e outras criaturas poderosas, é um excelente escalador e possui medo do subterrâneo e de ciclopes, e por isso não se dava bem com Tyson no início.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Grover_Underwood"
+    },
+    {
+      imagem: "https://i.pinimg.com/originals/b5/68/7d/b5687d94b70eab3a49d7664ea3cb6bd0.jpg",
+      titulo: "Tyson",
+      descricao: "Tyson é um ciclope, sendo filho de Poseidon e meio-irmão de Percy. Um dos poucos ciclopes bons, gentil, doce, infantil, chorão. Possui grande habilidades de contrução, só usa força bruta para salvar os amigos e é muito inteligente. É um dos poucos ciclopes realmente reconhecidos e reclamados por Poseidon.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Tyson"
+    },
+    {
+      imagem: "https://pm1.aminoapps.com/6508/432d26b81164c627a14d8cb2ea47a326f4a2dd81_128.jpg",
+      titulo: "Sra. O' Leary/ Sra O' leary",
+      descricao: "Único cão infernal amigável existente. Seu dono inicialmente era Quintus (Dédalo), mas depois da morte de Dédalo o seu dono virou Percy. Ela é filha de Cérbero e Nix.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Sra.O%27Leary"
+    },
+    {
+      imagem: "https://camp-chb.weebly.com/uploads/1/9/5/0/19500693/112650710.png",
+      titulo: "Cães infernais",
+      descricao: "Eles são originários dos Campos de Punição. Esses cães, podem farejar qualquer tipo de meio-sangue e são servos de Hades. São filhos de Cérbero e Nix.",
+      link: "https://acampamentomeio-sangue.fandom.com/pt-br/wiki/C%C3%A3o_Infernal"
+    },
+    {
+      imagem: "https://i.pinimg.com/564x/c2/a2/4c/c2a24cdf1e3e1928dfeba217ae875675.jpg",
+      titulo: "Empousai (Empusa ou Empousa)",
+      descricao: "Filhas da deusa Hécate e do espírito Mormo, possuem cabelos flamejantes, uma perna de bode e outra perna de Bronze celestial. As Empusas são enviadas por Hécate para vigiar estradas e devorar viajantes. Criadas pela combinação de magia negra, animal, bronze e fantasma.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Empusa"
+    },
+    {
+      imagem: "https://i.pinimg.com/736x/93/11/51/93115197640ea3960ab48447ce7f4324.jpg",
+      titulo: "Píton/Python/Piton",
+      descricao: "Serpente monstruosa que foi morta por Apolo e foi revivida por Gaia para destruir o oráculo de Delfos. Nasceu quando Zeus inundou o mundo para punir os humanos. Quando Leto foi na Terra procurando um lugar para dar à luz depois que Hera a amaldiçoou, Python a expulsou.",
+      link: "https://riordan.fandom.com/wiki/Python"
+    },
+    {
+      imagem: "https://pm1.aminoapps.com/7184/0082ff2eb8218cbb11ae2d876cad9cc602f2b629r1-591-475v2_00.jpg",
+      titulo: "Mymerkos",
+      descricao: "Formigas gigantes com carcaças extremamente resistentes e rubras. Suas pinças são usadas como armas, secretando veneno e uma gosma pegajosa. Habitam fortalezas ou subterrâneos. São atraídas por metais, principalmente ouro.",
+      link: "https://aminoapps.com/c/percy-jackson-br/page/item/myrmekos/wkNm_vTpIJa6GLrQKm2jk2404dxpQl4qb"
+    },
+    {
+      imagem: "https://i.pinimg.com/736x/58/64/53/586453a99ff8bcfad8d546eedae8d185.jpg",
+      titulo: "Jason Grace",
+      descricao: "Filho de Júpiter (contraparte romano de Zeus) e Beryl Grace, é irmão mais novo de Thalia Grace. Pode controlar os ventos. Foi treinado por Lupa e virou pretor do acampamento romano.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Jason_Grace"
+    },
+    {
+      imagem: "https://i.pinimg.com/564x/ca/ab/81/caab8106e423574c6add3d6719753b64.jpg",
+      titulo: "Frank Zhang",
+      descricao: "Filho de Marte (contraparte romano de Ares) com a mortal Emily Zhang, possui alergia a lactose e por muito tempo teve sua vida ligada à um graveto. É um descendente de Periclimeno, um príncipe grego neto de Poseidon, sendo um semideus e um legado. abençoado com a habilidade de mudar de forma da sua família, além de suas habilidades como um filho de Marte. Primeiro semideus conhecido a não possuir TDAH e dislexia.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Frank_Zhang"
+    },
+    {
+      imagem: "https://i.pinimg.com/474x/e2/99/29/e29929eb4257c79a1a829ca767088d89.jpg",
+      titulo: "Hazel Levesque",
+      descricao: "Filha de Plutão (contraparte romana de Hades) e Marie Levesque. É meia-irmã de Nico e Biancaa Di Angelo. Pode controlar pedras preciosas e morreu em 1941, e iria para os campos Elísios, mas se sacrificou pela mãe, aceitando a punição dela em troca de deixá-la ilesa. Reviveu 70 anos depois, quando Nico Di Angelo a encontrou enquanto procurava Bianca Di Angelo.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Hazel_Levesque#Vida_Pregressa"
+    },
+    {
+      imagem: "https://i.pinimg.com/564x/d9/04/8b/d9048b12ce8847aac70937b452ca2d5e.jpg",
+      titulo: "Reyna Avila",
+      descricao: "Reyna Avila Ramírez-Arellano é uma semideusa filha de Belona e irmã de Hylla Ramírez-Arellano, a atual rainha das Amazonas. Reyna foi pretora do Acampamento Júpiter por quatro anos, até se aposentar de seu cargo e jurar lealdade a Ártemis, se juntando as caçadoras. É sempre acompanhada por dois cachorros autômatos feitos de ouro e prata, respectivamente Aurum e Argentium.",
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Reyna_Avila_Ram%C3%ADrez-Arellano"
+    },
+    {
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl_lkWCjgAcgy7ENu_eynjTXy-PSTdiC-TQQ&s",
+      titulo: "Sally Jackson",
+      descricao:"Mãe de Percy Jackson, é uma mulher gentil e uma excelente mãe. Inventou o lance da comida azul com o Percy. Após se 'separar' de Gabe conheceu Paul, com quem se casou e agora possui uma filha chamada Estelle." ,
+      link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Sally_Jackson"
+    },
+  ];
